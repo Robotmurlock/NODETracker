de6205f589c8d8f629a218a2424e8db339a2e2f0
momiradzemovic <everseen>
2023-02-04 11:07:02 +0100

    Add (code): KF eval now performs multistep predictions

--- nodetracker/analysis_tools/run_evaluate_kf.py
+++ nodetracker/analysis_tools/run_evaluate_kf.py
@@ -6,6 +6,7 @@ import json
 import logging
 import os
 from pathlib import Path
+from collections import defaultdict
 
 from tqdm import tqdm
 
@@ -28,11 +29,13 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('--output-path', type=str, required=False, default=OUTPUTS_PATH, help='Output path')
     parser.add_argument('--split', type=str, required=False, default='val', help='Dataset split name')
     parser.add_argument('--dataset-name', type=str, required=True, help='Dataset name')
+    parser.add_argument('--steps', type=int, required=False, default=1, help='Number of forecast steps')
     return parser.parse_args()
 
 
 def main(args: argparse.Namespace) -> None:
     dataset_path = os.path.join(args.input_path, args.dataset_name, args.split)
+    n_pred_steps = args.steps
     logger.info(f'Loading dataset from path "{dataset_path}"')
     dataset = MOTDataset(
         path=dataset_path,
@@ -41,66 +44,64 @@ def main(args: argparse.Namespace) -> None:
         label_type=LabelType.GROUND_TRUTH
     )
 
-    mse_results = []
-    iou_results = []
+    sample_metrics = defaultdict(list)
 
     Path(args.output_path).mkdir(parents=True, exist_ok=True)
-    inference_path = os.path.join(args.output_path, 'kf_inference.csv')
     metrics_path = os.path.join(args.output_path, 'kf_metrics.json')
+    logger.info('Evaluating Kalman Filter')
 
-    logger.info('Writing inference data.')
-    with open(inference_path, 'w', encoding='utf-8') as writer:
-        header = 'scene,object_id,gt_ymin,gt_xmin,gt_w,gt_h,pred_ymin,pred_xmin,pred_w,pred_h'
-        writer.write(header)
+    scene_names = dataset.scenes
+    for scene_name in scene_names:
+        object_ids = dataset.get_scene_object_ids(scene_name)
+        for object_id in tqdm(object_ids, unit='track', desc=f'Evaluating tracker on {scene_name}'):
+            _, object_scene_id = dataset.parse_object_id(object_id)
+            kf = BotSortKalmanFilter()  # Create KF for new track
+            mean, covariance, mean_hat, covariance_hat = None, None, None, None
+            gt, gt_ymin, gt_xmin, gt_w, gt_h = None, None, None, None, None
 
-        scene_names = dataset.scenes
-        for scene_name in scene_names:
-            object_ids = dataset.get_scene_object_ids(scene_name)
-            for object_id in tqdm(object_ids, unit='track', desc=f'Evaluating tracker on {scene_name}'):
-                _, object_scene_id = dataset.parse_object_id(object_id)
-                kf = BotSortKalmanFilter()  # Create KF for new track
-                first_iteration = True
-                mean, covariance, mean_hat, covariance_hat = None, None, None, None
-                gt, gt_ymin, gt_xmin, gt_w, gt_h = None, None, None, None, None
+            n_data_points = dataset.get_object_data_length(object_id)
+            for index in range(n_data_points):
+                measurement = dataset.get_object_data_label(object_id, index)['bbox']
 
-                n_data_points = dataset.get_object_data_length(object_id)
-                for index in range(n_data_points):
-                    measurement = dataset.get_object_data_label(object_id, index)['bbox']
+                if mean is None:
+                    # First KF iteration
+                    mean, covariance = kf.initiate(measurement)
+                    pred, _ = kf.project(mean, covariance)
+                    # Nothing to compare in first iteration
+                else:
+                    # Perform prediction
+                    mean_hat, covariance_hat = kf.predict(mean, covariance)
+                    mean, covariance = kf.update(mean_hat, covariance_hat, measurement)
 
-                    if mean is None:
-                        # First KF iteration
-                        mean, covariance = kf.initiate(measurement)
-                        pred, _ = kf.project(mean, covariance)
-                    else:
-                        mean_hat, covariance_hat = kf.predict(mean, covariance)
-                        mean, covariance = kf.update(mean_hat, covariance_hat, measurement)
-                        pred, _ = kf.project(mean_hat, covariance_hat)
+                    total_mse = 0.0
+                    total_iou = 0.0
+                    for p_index in range(1, n_pred_steps+1):
+                        pred, _ = kf.project(mean_hat, covariance_hat)  # KF Prediction is before update
 
-                    if not first_iteration:
-                        # Nothing to compare in first iteration
+                        # Calculate MSE
+                        mse = ((gt - pred) ** 2).mean()
+                        sample_metrics[f'MSE-{p_index}'].append(mse)
+                        total_mse += mse
+
+                        # Calculate IOU (Accuracy)
                         pred_ymin, pred_xmin, pred_w, pred_h = pred
                         gt_bbox = BBox.from_xyhw(gt_xmin, gt_ymin, gt_h, gt_w, clip=True)
                         pred_bbox = BBox.from_xyhw(pred_xmin, pred_ymin, pred_h, pred_w, clip=True)
 
-                        mse = ((gt - pred) ** 2).mean()
                         iou_score = gt_bbox.iou(pred_bbox)
-                        mse_results.append(mse)
-                        iou_results.append(iou_score)
+                        sample_metrics[f'Accuracy-{p_index}'].append(iou_score)
+                        total_iou += iou_score
 
-                        row_data = \
-                            f'\n{scene_name},{object_scene_id},{index},{gt_ymin},{gt_xmin},{gt_w},{gt_h},{pred_ymin},{pred_xmin},{pred_w},{pred_h}'
-                        writer.write(row_data)
-                    else:
-                        first_iteration = False
+                        mean_hat, covariance_hat = kf.predict(mean_hat, covariance_hat)
 
-                    # Ground truth values need to be 1 step behind in order to compare with predictions
-                    gt_ymin, gt_xmin, gt_w, gt_h = measurement
-                    gt = measurement.copy()
+                    sample_metrics['MSE'].append(total_mse / n_pred_steps)
+                    sample_metrics['Accuracy'].append(total_iou / n_pred_steps)
 
-    metrics = {
-        'mse': sum(mse_results) / len(mse_results),
-        'avg_iou': sum(iou_results) / len(iou_results)
-    }
+                # Ground truth values need to be 1 step behind in order to compare with predictions
+                gt_ymin, gt_xmin, gt_w, gt_h = measurement
+                gt = measurement.copy()
+
+    metrics = {k: sum(v) / len(v) for k, v in sample_metrics.items()}  # Metrics aggregation (averaging)
     logger.info(f'Metrics: \n{json.dumps(metrics, indent=2)}')
 
     with open(metrics_path, 'w', encoding='utf-8') as f:
