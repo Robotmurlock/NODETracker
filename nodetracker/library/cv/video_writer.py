ad9dabc605fe879f4f69dec822881b340d555194
momiradzemovic <everseen>
2023-04-23 13:34:45 +0200

    Add (code): Update MP4Writer

--- nodetracker/library/cv/video_writer.py
+++ nodetracker/library/cv/video_writer.py
@@ -1,67 +1,116 @@
 """
-MP4Writer support
+RAII wrapper of mp4 writer.
 """
-from typing import Tuple
+from pathlib import Path
+from typing import Tuple, Optional
 
 import cv2
 import numpy as np
 
 
+class MP4WriterResolutionMismatchException(Exception):
+    """
+    Frame resolution does not match with the previously used one
+    """
+    pass
+
+
+class MP4WriterNotOpen(Exception):
+    """
+    MP4Writer is not open yet
+    """
+    pass
+
+
 class MP4Writer:
     """
-    RAII implementation of cv2.VideoWriter
+    RAII MP4 writer
     """
-    def __init__(self, path: str, fps: int, resolution: Tuple[int, int], resize: bool = True):
+    # noinspection PyUnresolvedReferences
+    def __init__(
+        self,
+        path: str,
+        fps: int,
+        shape: Optional[Tuple[int, int]] = None,
+        resize: bool = False,
+        resize_interpolation: int = cv2.INTER_NEAREST
+    ):
         """
         Args:
-            path: video path
-            fps: video fps
-            resolution: video resolution
-            resize: resize input images
+            path: Output video path
+            fps: Output video fps
+            shape: Set video output shape (optional) - Format (w, h)
+                - if not set then first video frame resolution will be taken as shape
+            resize: Resize video to some shape (requires shape to be set)
         """
-        self._path = path
         self._fps = fps
-        self._resolution = resolution
+        self._path = path
         self._resize = resize
+        self._resize_interpolation = resize_interpolation
 
         # State
-        self._writer = None
+        self._writer = None  # Stored on first frame
+        self._shape: Optional[Tuple[int, int]] = shape  # Stored on first frame if not set
 
-    def open(self) -> None:
+        # Check
+        if self._resize and not self._shape:
+            raise AttributeError('Shape has to be set with the "resize" option!')
+
+    @property
+    def fps(self) -> int:
         """
-        Open video for writing.
+        Gets video fps.
+
+        Returns:
+            fps
         """
-        # noinspection PyUnresolvedReferences
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        # noinspection PyUnresolvedReferences
-        self._writer = cv2.VideoWriter(self._path, fourcc, self._fps, self._resolution)
+        return self._fps
+
+    @property
+    def resolution(self) -> Tuple[int, int]:
+        """
+        Returns:
+            Video resolution
+        """
+        if self._shape is None:
+            raise MP4WriterNotOpen('Shape is unknown. Please use write() at least once.')
+        return self._shape
+
+    def write(self, frame: np.ndarray) -> None:
+        """
+        Appends new frame to mp4 video.
+
+        Args:
+            frame: Frame to write to video
+        """
+        h, w, _ = frame.shape
+        shape = (w, h)
+        if self._writer is None:
+            if self._shape is None:
+                self._shape = shape
+
+            # noinspection PyUnresolvedReferences
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            Path(self._path).parent.mkdir(parents=True, exist_ok=True)
+            # noinspection PyUnresolvedReferences
+            self._writer = cv2.VideoWriter(self._path, fourcc, self._fps, self._shape)
+
+        if shape != self._shape:
+            if not self._resize:
+                raise MP4WriterResolutionMismatchException(f'Resolution mismatch! Expected {self._shape} but got {shape}.')
+            else:
+                # noinspection PyUnresolvedReferences
+                frame = cv2.resize(frame, self._shape, interpolation=self._resize_interpolation)
+
+        self._writer.write(frame)
 
     def close(self) -> None:
         """
-        Close video for writing.
+        Closes mp4 writer.
         """
         self._writer.release()
-        self._writer = None
-
-    def write(self, image: np.ndarray) -> None:
-        """
-        Write image to video mp4.
-
-        Args:
-            image: Image
-        """
-        assert self._writer is not None, 'Writer not open.'
-        h, w, _ = image.shape
-
-        if (w, h) != self._resolution:
-            assert self._resize, f'Got image resolution {(w, h)} but expected {self._resolution}'
-            # noinspection PyUnresolvedReferences
-            image = cv2.resize(image, self._resolution)
-
-        self._writer.write(image)
 
     def __enter__(self) -> 'MP4Writer':
-        self.open()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
