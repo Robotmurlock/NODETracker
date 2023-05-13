13a339045e82c5efbd38e9ea1e1db80e04c4c61c
Robotmurlock <momir.adzemovic@gmail.com>
2023-05-13 15:23:22 +0200

    Add (code): Update LookupTable - class labels

--- nodetracker/utils/lookup.py
+++ nodetracker/utils/lookup.py
@@ -1,12 +1,21 @@
-from typing import Dict, Optional
+from typing import Dict, Optional, Iterable, Tuple, List
+
 
 class LookupTable:
+    """Vocabulary - Label lookup table (token <-> index)."""
+
     def __init__(
-        self,
-        token_to_index: Optional[Dict[str, int]] = None,
-        unknown_token: str = '<unk>',
-        add_unknown_token: bool = True
+            self,
+            token_to_index: Optional[Dict[str, int]] = None,
+            unknown_token: str = '<unk>',
+            add_unknown_token: bool = True
     ):
+        """
+        Args:
+            token_to_index: Predefine token to index mappings.
+            unknown_token: Unknown token value.
+            add_unknown_token: Use unknown token.
+        """
         self._token_to_index = token_to_index
         self._unknown_token = unknown_token
         self._add_unknown_token = add_unknown_token
@@ -21,6 +30,15 @@ class LookupTable:
         self._next_index = len(self._token_to_index)
 
     def add(self, token: str) -> int:
+        """
+        Adds token to the lookup table if it does not already exist.
+
+        Args:
+            token: Label (token)
+
+        Returns:
+            label (token) index
+        """
         if token in self._token_to_index:
             return self._token_to_index[token]
 
@@ -31,16 +49,49 @@ class LookupTable:
         return new_index
 
     def lookup(self, token: str) -> int:
+        """
+        Acquires token index if it exists in the table.
+        In case the token does not exist in the lookup table:
+            - and unknown token is used then unkown token index is returned;
+            - otherwise KeyError is raised
+
+        Raises:
+            KeyError: Unknown token
+
+        Returns:
+            label (token) index
+        """
         if token not in self._token_to_index and self._add_unknown_token:
             return self._token_to_index[self._unknown_token]
 
         return self._token_to_index[token]
 
     def inverse_lookup(self, index: int) -> str:
+        """
+        Inverse to `lookup`. Acquire token by index.
+
+        Raises:
+            KeyError: Unknown index
+
+        Returns:
+            label (token)
+        """
         return self._index_to_token[index]
 
+    def __iter__(self) -> Iterable[Tuple[str, int]]:
+        for token, index in self._token_to_index.items():
+            yield token, index
+
+    @property
+    def tokens(self) -> List[str]:
+        """
+        Returns:
+            List of tokens in lookup table
+        """
+        return [token for token, _ in self]
+
     def __getitem__(self, token: str) -> int:
-        return self.lookup(token)
+        return self.lookup(token)  # Alias for `lookup`
 
     def __len__(self):
         return self._next_index
@@ -66,6 +117,7 @@ def run_test() -> None:
     assert lookup['cat'] == lookup.lookup('cat') == 2
     assert lookup.inverse_lookup(1) == 'dog'
     assert lookup.inverse_lookup(2) == 'cat'
+    assert lookup.tokens == ['<unk>', 'dog', 'cat']
 
     deserialized = LookupTable.deserialize(lookup.serialize())
 
